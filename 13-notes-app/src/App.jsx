3ff92435d201dbefeec80da0e6b6191442007173
Rohit Kumar <147175132+rk7256955917@users.noreply.github.com>
2026-06-20 01:07:39 +0530

    change-notes-task

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,24 +1,64 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const App = () => {
+
+const [title, setTitle] = useState('')
+const [details, setDetails] = useState('')
+
+
 const submitHandler=(e)=>{
   e.preventDefault()
-  console.log('From submited')
+
+const copyTask = [...task];
+copyTask.push{copyTasky, setcopy] = useState('')
+
+  
+  setTitle('');
+  setDetails('');
 }
+
   return (
-    <div className='h-screen bg-black text-white'>
-      <h1 className='px-7 '>Add Notes</h1>
+
+
+    <div className='h-screen lg:flex bg-black text-white'>
       <form onSubmit={(e)=>{
         submitHandler(e);
-      }} className='flex justify-between items-starts p-10'>
-        <div classNa
-        <input className='w-80 px-5 py-2 border-2 rounded' type="text" placeholder='Enter Notes Heading'/>
-       <input className=' w-80 px-5 h-20 py-2 border-2 rounded' type="text" placeholder='Write Details'/>
-        <button className='h-full border-2 rounded text-black bg-white w-80'>Add Note</button>
-        <div className='h-full flex space-between '>
-        <div className=' flex h-screen w-80 h-10 border-2 bg-white '></div>
-        </div>
+
+      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-starts '>
+
+        <h1 className='text-3xl font-bold'>Add Notes</h1>
+
+        <input
+         type="text"
+         placeholder='Enter Notes Heading'
+        className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
+        value={title}
+        onChange={(e)=>{
+          setTitle(e.target.value);
+        }}
+        />
+
+
+       <textarea 
+        type="text"
+        className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none rounded'
+         placeholder='Write Details'
+        value={details}
+         onChange={(e)=>{
+         setDetails(e.target.value);
+         }}
+         />
+
+        <button className='bg-white font-medium w-full outline-none px-5 py-2 rounded text-black'>Add Note</button>
       </form>
+      <div className='lg:w-1/2 lg:border-l-2 px-10 '>
+      <h1 className='text-xl font-bold'>Recent Notes</h1>
+      <div className='flex flex-wrap gap-5 mt-5 overflow-auto'>
+        <div className='h-52 w-40 rounded-2xl bg-white '></div>
+        <div className='h-52 w-40 rounded-2xl bg-white '></div>
+        <div className='h-52 w-40 rounded-2xl bg-white '></div>
+        </div>
+      </div>
     </div>
   )
 }
