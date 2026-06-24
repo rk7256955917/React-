12c251f77e8e1a336d4e9781153ae523625f8eae
Rohit Kumar <147175132+rk7256955917@users.noreply.github.com>
2026-06-24 23:53:04 +0530

    Navbar

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -3,11 +3,17 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import { Route,Routes} from 'react-router-dom'
 
-
-
 const App = () => {
   return (
     <div>
+    <div className='nav'>
+      <h3>Sheryians</h3>
+      <div>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+      </div>
+      </div>
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
